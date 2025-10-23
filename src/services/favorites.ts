027fd71677608202512afa6f025dd9d0e08b55dc
Juan David Soto <juan.soto.garzon@correounivalle.edu.co>
2025-10-23 13:26:51 -0500

    Enruamiento de favoritos

--- src/services/favorites.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "./api";
-
-export const favoritesService = {
-  getAll: () => api.favorites.getAll(),
-  add: (favorite: { id: string; title: string; url: string; thumbnail: string }) =>
-    api.favorites.add(favorite),
-  remove: (id: string) => api.favorites.remove(id),
-};
